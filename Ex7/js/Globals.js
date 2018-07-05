6bbaf2b3e576e1a0bff6e14a8131d6d61b7abb62
volodymyr bondariev <vladmikhbond@hotmail.com>
2018-07-05 10:09:06 +0300

    Ex7

---
+++ Ex7/js/Globals.js
@@ -0,0 +1,9 @@
+let W = 0.5;  // потеря энергии при соударении  0.5
+let K = 0.1;  // модуль упругости
+
+let g = 0.05;  // 0.05;
+let INTERVAL = 30;
+let intervalId;
+
+let REPEATER = 10;
+let PRETTY = false;
